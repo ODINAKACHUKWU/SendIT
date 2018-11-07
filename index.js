ca031f98097b2aae54d0a9237faba8bf33668a81
Solomon Ezeobika <solomonzbk@gmail.com>
2018-11-07 13:27:14 +0100

    Add GET /parcels

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const _ = require('underscore');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+let parcels = [];
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('SendIT API Root');
+});
+
+// GET /parcels
+app.get('/parcels', (req, res) => {
+    res.json(parcels);
 });
 
 app.listen(PORT, () => {

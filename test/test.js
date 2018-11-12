49f6071179a6a8f94ecd1b2a5471bd9a10b7669e
Solomon Ezeobika <solomonzbk@gmail.com>
2018-11-12 23:39:00 +0100

    Recreate directory structure

--- test/test.js
+++ test/test.js
@@ -1,3 +1,43 @@
-process.env.NODE_ENV = 'test';
+import myApp from '../server/index';
 
-const myApp = require('./index.js');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Parcel API endpoints', () => {
+  it('should get all the parcels', (done) => {
+    chai.request(myApp)
+      .get('/api/v1/parcels')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.should.have.lengthOf(3);
+        done();
+      });
+  });
+
+  it('should get a parcel by id', (done) => {
+    chai.request(myApp)
+      .get('/api/v1/parcels/:id')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+
+  it('should delete a parcel by id', (done) => {
+    chai.request(myApp)
+      .get('/api/v1/parcels/:id/delete');
+    done();
+  });
+
+  it('should cancel a parcel by id', (done) => {
+    chai.request(myApp)
+      .get('/api/v1/parcels/:id/cancel');
+    done();
+  });
+});

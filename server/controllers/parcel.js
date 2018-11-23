808158dea6af777d7347a8d5b446a41744cf497a
Solomon Ezeobika <solomonzbk@gmail.com>
2018-11-23 09:51:12 +0100

    update parcels routes

--- server/controllers/parcel.js
+++ server/controllers/parcel.js
@@ -86,49 +86,78 @@ class ParcelController {
       });
   }
 
-  // only user who created the parcel can change the destination
+  /**
+   * only user who created the parcel can change the destination
+   *  */
   static changeDestination(req, res) {
     const id = parseInt(req.params.id, 10);
-    const { destination } = req.body;
+    const { destination, decoded } = req.body;
 
-    pool.query(
-      'UPDATE parcels SET destination = $1 WHERE id = $2',
-      [destination, id],
-      (error, results) => {
-        if (error) {
-          return res.status(500).send({
-            status: 'Failure',
-            message: error.message,
-          });
-        }
-
-        const parcel = results.rows[0];
-
-        if (!parcel) {
-          return res.status(404).send({
-            status: 'Failure',
-            message: 'Parcel not found',
-          });
-        }
-
-        return res.status(200).send({
-          status: 'Success',
-          message: 'Destination has been changed',
-          data: parcel,
+    pool.query('SELECT * FROM parcels WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        return res.status(500).send({
+          status: 'Failure',
+          message: error.message,
         });
-      },
-    );
+      }
+
+      const parcel = results.rows[0];
+
+      if (parcel.userid !== decoded.userId) {
+        return res.status(403).send({
+          status: 'Failure',
+          message: 'The parcel delivery order was not created by you',
+        });
+      }
+
+      if (!parcel) {
+        return res.status(404).send({
+          status: 'Failure',
+          message: 'Parcel not found',
+        });
+      }
+
+      if (parcel.order_status === 'Delivered') {
+        return res.status(403).send({
+          status: 'Failure',
+          message: 'Parcel has been delivered',
+        });
+      }
+
+      pool.query(
+        'UPDATE parcels SET destination = $1 WHERE id = $2',
+        [destination, id],
+        (err, results) => {
+          if (err) {
+            return res.status(500).send({
+              status: 'Failure',
+              message: err.message,
+            });
+          }
+
+          const updatedParcel = results.rows[0];
+
+          return res.status(200).send({
+            status: 'Success',
+            message: 'Destination has been changed',
+            data: updatedParcel,
+          });
+        },
+      );
+    });
   }
 
   /**
-   * only admin can change the status to either delivered or cancelled
+   * only admin can change the status to delivered
    *  */
   static changeStatus(req, res) {
     const id = parseInt(req.params.id, 10);
     const { status, decoded } = req.body;
 
+    console.log(decoded, '-----------------------');
+
     if (decoded.category !== 'Admin') {
-      return res.status(401).send({
+      return res.status(403).send({
         status: 'Failure',
         message: 'You are not an admin',
       });
@@ -150,20 +179,27 @@ class ParcelController {
           message: 'Parcel not found',
         });
       }
-      console.log(parcel);
+
+      if (parcel.order_status === 'Delivered') {
+        return res.status(403).send({
+          status: 'Failure',
+          message: 'Parcel has been delivered',
+        });
+      }
 
       pool.query(
-        'UPDATE parcels SET order_status = $1 WHERE id = $2',
-        [status, decoded, id],
-        (error, results) => {
-          if (error) {
+        'UPDATE parcels SET order_status = $1 WHERE id = $2 RETURNING *',
+        [status, id],
+        (err, results) => {
+          if (err) {
             return res.status(500).send({
               status: 'Failure',
-              message: error.message,
+              message: err.message,
             });
           }
 
-          // const parcel = results.rows[0];
+          const updatedParcel = results.rows[0];
+          console.log(updatedParcel, '--------------');
 
           // if (!parcel) {
           //   return res.status(404).send({
@@ -172,11 +208,13 @@ class ParcelController {
           //   });
           // }
 
-          return res.status(200).send({
-            status: 'Success',
-            message: 'Status has been changed',
-            data: parcel,
-          });
+          //   return res.status(200).send({
+          //     status: 'Success',
+          //     message: 'Status has been changed',
+          //     data: parcel,
+          //   });
+          //   // },
+          // );
         },
       );
     });
@@ -185,35 +223,106 @@ class ParcelController {
   // only admin can change the location
   static changeLocation(req, res) {
     const id = parseInt(req.params.id, 10);
-    const { location } = req.body;
+    const { location, decoded } = req.body;
 
-    pool.query(
-      'UPDATE parcels SET present_location = $1 WHERE id = $2',
-      [location, id],
-      (error, results) => {
-        if (error) {
-          return res.status(500).send({
-            status: 'Failure',
-            message: error.message,
-          });
-        }
+    console.log(decoded, '-----------------------');
 
-        const parcel = results.rows[0];
+    if (decoded.category !== 'Admin') {
+      return res.status(403).send({
+        status: 'Failure',
+        message: 'You are not an admin',
+      });
+    }
 
-        if (!parcel) {
-          return res.status(404).send({
-            status: 'Failure',
-            message: 'Parcel not found',
-          });
-        }
-
-        return res.status(200).send({
-          status: 'Success',
-          message: 'Location has been changed',
-          data: parcel,
+    pool.query('SELECT * FROM parcels WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        return res.status(500).send({
+          status: 'Failure',
+          message: error.message,
         });
-      },
-    );
+      }
+
+      const parcel = results.rows[0];
+
+      if (!parcel) {
+        return res.status(404).send({
+          status: 'Failure',
+          message: 'Parcel not found',
+        });
+      }
+
+      if (parcel.order_status === 'Delivered') {
+        return res.status(403).send({
+          status: 'Failure',
+          message: 'Parcel has been delivered',
+        });
+      }
+
+      pool.query(
+        'UPDATE parcels SET present_location = $1 WHERE id = $2 RETURNING *',
+        [location, id],
+        (err, results) => {
+          if (err) {
+            return res.status(500).send({
+              status: 'Failure',
+              message: err.message,
+            });
+          }
+
+          const updatedParcel = results.rows[0];
+          console.log(updatedParcel, '--------------');
+
+          // if (!parcel) {
+          //   return res.status(404).send({
+          //     status: 'Failure',
+          //     message: 'Parcel not found',
+          //   });
+          // }
+
+          //   return res.status(200).send({
+          //     status: 'Success',
+          //     message: 'Status has been changed',
+          //     data: parcel,
+          //   });
+          //   // },
+          // );
+        },
+      );
+    });
+
+
+
+    //   const id = parseInt(req.params.id, 10);
+    //   const { location } = req.body;
+
+    //   pool.query(
+    //     'UPDATE parcels SET present_location = $1 WHERE id = $2',
+    //     [location, id],
+    //     (error, results) => {
+    //       if (error) {
+    //         return res.status(500).send({
+    //           status: 'Failure',
+    //           message: error.message,
+    //         });
+    //       }
+
+    //       const parcel = results.rows[0];
+
+    //       if (!parcel) {
+    //         return res.status(404).send({
+    //           status: 'Failure',
+    //           message: 'Parcel not found',
+    //         });
+    //       }
+
+  //       return res.status(200).send({
+  //         status: 'Success',
+  //         message: 'Location has been changed',
+  //         data: parcel,
+  //       });
+  //     },
+  //   );
+  // }
   }
 }
 
